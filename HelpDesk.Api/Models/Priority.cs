6aa214548b8bb4bae233a76f3fc5f2b360361cc7
abhishek-5721 <anandabhishek5721@gmail.com>
2026-08-03 15:27:02 +0530

    Initial  Commit

---
+++ HelpDesk.Api/Models/Priority.cs
@@ -0,0 +1,9 @@
+﻿namespace HelpDesk.Api.Models
+{
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+}
---
+++ HelpDesk.Api/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDesk.Api.Models
+{
+    public enum Status
+    {
+        Open,
+
+        [Display(Name = "In Progress")]
+        In_Progress,
+
+        Class
+    }
+}
